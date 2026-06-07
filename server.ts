9e192ab149ca4c967243b7264766e6b5b0175d97
Josphine Ndungu <josphinendungu42@gmail.com>
2026-06-07 00:24:41 -0400

    Use Render PORT environment variable

--- server.ts
+++ server.ts
@@ -11,7 +11,7 @@ dotenv.config();
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = Number(process.env.PORT) || 3000;
 
   // Body parsers
   app.use(express.json());
